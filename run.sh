c5167f3d8984131d45d56d3dd666169638f90f21
Poma <Semenov.Roman@mail.ru>
2017-05-22 10:40:29 +0300

    chef package link

--- run.sh
+++ run.sh
@@ -3,11 +3,8 @@ SCRIPT_PATH="`dirname \"$0\"`"
 
 if ! type "chef-solo" > /dev/null || ! type "berks" > /dev/null; then
 	echo "Installing Chef-DK"
-	apt-get install -y wget
-	wget https://packages.chef.io/stable/ubuntu/12.04/chefdk_0.16.28-1_amd64.deb
-	dpkg -i chefdk_0.16.28-1_amd64.deb
-	apt-get install -f
-	rm chefdk_0.16.28-1_amd64.deb
+	apt-get install -y curl
+	curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -c current -P chefdk
 fi
 
 #berks install
